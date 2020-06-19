4abc8e89563518d8b7652372439f941a59ef3bc4
KelvinCosta <kelvinvicentecosta@gmail.com>
2020-06-18 21:18:13 -0300

    Catalog e Control unificados uma vez que não há necessidade de form

--- movie/src/main/java/interview/movie/Catalog.java
+++ movie/src/main/java/interview/movie/Catalog.java
@@ -5,22 +5,38 @@
  */
 package interview.movie;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 /**
  *
  * @author KelvinVicenteCosta
  */
-
+@RestController
+@RequestMapping("/filmes")
 public class Catalog {    
     
     RestTemplate restTemplate = new RestTemplate();
     private final String URL = "https://jsonmock.hackerrank.com/api/movies/search/";
     
-    Object getMovies(String substr, String pageNumber) {
+    private Object getMovies(String substr, String pageNumber) {
         String searchedUrl = URL + "?Title=" + substr + "&page=" + pageNumber;
         Object response = restTemplate.getForObject(searchedUrl, Object.class);
         return response;
+    }
+    
+    @RequestMapping("/{movieName}")
+    public Object showMovie (@PathVariable("movieName") String movieName) {
+        return getMovies(movieName, "1");
+    }
+    
+    @RequestMapping("/{movieName}/{pageNumber}")
+    public Object movieAndPage(
+            @PathVariable("movieName") String movieName, 
+            @PathVariable("pageNumber") String pageNumber) {        
+        return getMovies(movieName, pageNumber);
     }
     
 }
